8d95dddd1783ab298fa1b17dab4798ab16f2e66d
Nothing <nothing@nothing.local>
2017-08-16 00:21:09 +0800

    add postReducer to reducer

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -4,12 +4,13 @@ import AuthReducer from './authReducer'
 import OAuthReducer from './oAuthReducer'
 import RouterReducer from './routReducer'
 import {routerReducer} from 'react-router-redux'
-
+import PostReducer from './postReducer'
 const reducer = combineReducers({
   user:UserReducer,
   auth:AuthReducer,
   routing:RouterReducer,
   router:routerReducer,
+  post : PostReducer,
 })
 
 export default reducer;
