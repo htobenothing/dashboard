e43abf3b389d0e49fdfb12278f3284bc235834f0
Nothing <nothing@nothing.local>
2017-08-16 00:18:07 +0800

    add create post action

--- src/actions/postAction.js
+++ src/actions/postAction.js
@@ -1,11 +1,20 @@
 import * as C from './constants'
 import axios from 'axios'
 
+export const CREATE_POST_START = "CREATE_POST_START"
+export const CREATE_POST_SCUCCED = "CREATE_POST_SUCCED"
+export const CREATE_POST_FAILED = "CREATE_POST_FAILED"
 
+export function Create_Post_Start(post){
+  return dispatch=>{
+    dispatch({type:CREATE_POST_START,payload:post})
 
+    console.log('post save success',post)
+    // post 'post'  
+  }
+}
 
-
-function Fetch_Post_Start(params) {
+export function Fetch_Post_Start(params) {
 
   return dispatch => {
     dispatch({type:C.FETCH_POST_START})
