27ed199bd7fe2e240e7241172d88821addfa4328
Nothing <nothing@nothing.local>
2018-01-06 23:09:20 +0800

    Updates

--- src/actions/routerAction.js
+++
@@ -1,6 +0,0 @@
-
-import * as C from '../actions/constants'
-export function redirectTo(url){
-  return({type:C.REDIRECT, payload:url})
-}
-
--- src/actions/userActions.js
+++
@@ -1,9 +0,0 @@
-import {FETCH_USER,FETCH_USER_SUCCED,FETCH_USER_FAILED} from './constants'
-
-export async function Fetch_User() {
-  (dispatch)=>{
-
-    dispatch({type:FETCH_USER})
-
-  }
-}
